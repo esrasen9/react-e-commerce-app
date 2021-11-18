5d5ebcec675c60e3247b1edff652d1c07257a118
esrasen9 <esrasen860@gmail.com>
2021-11-18 22:44:02 +0300

    Payment page components created.

--- src/components/pages/payment/Payment.js
+++ src/components/pages/payment/Payment.js
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import {useStateValue} from "../../../context/Context";
+import "./Payment.css";
+import Address from "./Address";
+import PaymentMethod from "./PaymentMethod";
+import CartProduct from "../cart/CartProduct";
 const Payment = () =>  {
+    const [initialState] = useStateValue();
+    const {cart} = initialState;
     return (
-        <div>
-şrefvşkoş3kvrf
+        <div className="page payment-page">
+            <div>
+                <h1>Checkout ({cart.length} items)</h1>
+                {
+                    cart && cart.map((product,index) =>{
+                        return(
+                            <div>
+                                <CartProduct
+                                    id={product.id}
+                                    key={index}
+                                    title={product.title}
+                                    image={product.image}
+                                    price={product.price}
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="payment-details">
+                <Address/>
+                <PaymentMethod />
+           </div>
         </div>
     );
 }
